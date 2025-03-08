45e60ba5551a16fe9519dd3d868c0da2eebb81c9
sb1709 <saurav.bisht1709@gmail.com>
2025-03-08 10:09:08 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -32,8 +32,8 @@ def get_requirements()->List[str]:
 setup(
     name="NetworkSecurity",
     version="0.0.1",
-    author="Krish Naik",
-    author_email="krishnaik06@gmail.com",
+    author="Saurav Bisht",
+    author_email="Saurav.bisht1709@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements()
 )
